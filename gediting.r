571c578e90957622e3ab736ce2919f378b2445b8
pajanne <pajanne@gmail.com>
2017-03-15 19:05:01 +0000

    merged clone growth data but plot is not there yet

--- gediting.r
+++ gediting.r
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-# read data
+# read protein abundance data from InCellWestern
 protein_abundance_data <- read.csv('data/20170118-GED-EXP-00000001-ICW.csv')
 protein_abundance_data
 
@@ -19,4 +19,21 @@ protein_abundance_plot <- ggplot(protein_abundance_data, aes(x = Target, y = rat
   theme(axis.text.x  = element_text(angle=15, vjust=0.5, size=10))
 
 print(protein_abundance_plot)
+
+# read clone growth data
+clone_growth_data <- read.csv('data/20170118-GED-EXP-00000002-ICC.csv')
+clone_growth_data
+
+# plot clone growth curves
+clone_growth_curve <-  ggplot(clone_growth_data, aes(x = X0, y = Target, group = Well)) +
+  geom_line(aes(colour = Target)) +
+  theme_bw() +
+  ggtitle('Clone growth curves') +
+  xlab('Elapse time') +
+  ylab('Confluence (%)') +
+  theme(axis.text.x = element_text(angle=15, vjust=0.5, size=10))
+
+print(clone_growth_curve)
+
+# plot clone growth rate
 
