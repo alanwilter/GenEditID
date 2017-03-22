089343dbac34583421a5c42c7d900377ae538c3f
pajanne <pajanne@gmail.com>
2017-03-22 15:16:37 +0000

    improve growth curves following discussion with @rubenalv

--- gediting.r
+++ gediting.r
@@ -38,7 +38,6 @@ protein_abundance_plot <- ggplot(protein_abundance_data, aes(x = Target, y = rat
 print(protein_abundance_plot)
 
 # read clone growth data
-#clone_growth_data <- read.csv('data/20170118-GED-EXP-00000002-ICC.csv')
 clone_growth_data <- left_join(well, clone, by="clone_id") %>% 
   left_join(growth, well, by="well_id") %>% 
   collect %>% 
@@ -50,15 +49,16 @@ clone_growth_data <- left_join(well, clone, by="clone_id") %>%
 clone_growth_data 
 
 # plot clone growth curves
-clone_growth_curve <-  ggplot(clone_growth_data, aes(x = Elapsed, y = Confluence)) +
-  geom_smooth(aes(colour = Target)) +
+clone_growth_curve <-  ggplot(clone_growth_data, aes(x = Elapsed, y = Confluence, group = as.factor(Target):as.factor(PlateID):as.factor(Well))) +
+  geom_line(aes(colour = Target)) +
   theme_bw() +
   ggtitle('Clone growth curves') +
   xlab('Elapse time') +
-  ylab('Confluence (%)') +
-  theme(axis.text.x = element_text(angle=15, vjust=0.5, size=10)) + facet_wrap(~PlateID)
+  ylab('Confluence (%)')
 
 print(clone_growth_curve)
+
+# TODO create one plot per well with plotly
 
 # calculate slope of growth curves with grofit
 # TODO
