e79ad6377d6506906e9528370db1955a88daada9
pajanne <pajanne@gmail.com>
2017-03-24 00:15:36 +0000

    Improved model following discussion with @rubenalv and @rich7409
    Add new tables for Amplicons, Primers, WellContents, SequencingLibrary,
    and ExperimentLayout.

--- python/dnascissors/model.py
+++ python/dnascissors/model.py
@@ -5,7 +5,7 @@ Created by Anne Pajon @pajanne on 08/03/2017
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import backref
-from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean, Enum, ForeignKey, UniqueConstraint
+from sqlalchemy import Table, Column, Integer, String, DateTime, Float, Boolean, Enum, ForeignKey, UniqueConstraint
 
 Base = declarative_base()
 
@@ -15,6 +15,17 @@ class Project(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(64), unique=True, nullable=False, index=True)
     description = Column(String(1024))
+
+
+class ExperimentLayout(Base):
+    __tablename__ = 'experiment_layout'
+    id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey('project.id', name='experiment_layout_project_fk', ondelete='cascade'))
+    project = relationship(
+        Project,
+        backref=backref('experiment_layouts', uselist=True, cascade='delete,all'))
+    name = Column(String(32), index=True)
+    description = Column(String)
 
 
 class Target(Base):
@@ -39,13 +50,51 @@ class Guide(Base):
     target = relationship(
         Target,
         backref=backref('guides', uselist=True, cascade='delete,all'))
+    amplicons = relationship("Amplicon", back_populates="guide")
     name = Column(String(32), nullable=False, index=True)
     description = Column(String(1024))
     location = Column(Integer, nullable=False)
     strand = Column(String(1), nullable=False)
     guide_sequence = Column(String(250), nullable=False)
     pam_sequence = Column(String(6), nullable=False)
-    signature_protein = Column(String(250), nullable=False)
+    nuclease = Column(String(250), nullable=False)
+
+
+class AmpliconSelection(Base):
+    __tablename__ = 'amplicon_selection'
+    guide_id = Column(Integer, ForeignKey('guide.id'), primary_key=True)
+    guide = relationship("Guide", back_populates="amplicons")
+    amplicon_id = Column(Integer, ForeignKey('amplicon.id'), primary_key=True)
+    amplicon = relationship("Amplicon", back_populates="guides")
+    name = Column(String(32), unique=True, nullable=False, index=True)
+    description = Column(String(1024))
+    score = Column(Integer)
+    experiment_type = Column(String(32))
+    donor_sequence = Column(String(250), nullable=True)
+
+
+class Amplicon(Base):
+    __tablename__ = 'amplicon'
+    id = Column(Integer, primary_key=True)
+    guides = relationship("Guide", back_populates="amplicon")
+    name = Column(String(32), unique=True, nullable=False, index=True)
+    description = Column(String(1024))
+    target = Column(Enum('On', 'Off'))
+    target_type = Column(Enum('gene', 'precursor', 'non-coding'))
+    chromosome = Column(String(32), nullable=False, index=True)
+    start = Column(Integer, nullable=False)
+    end = Column(Integer, nullable=False)
+
+
+class Primer(Base):
+    __tablename__ = 'primer'
+    id = Column(Integer, primary_key=True)
+    amplicon_id = Column(Integer, ForeignKey('amplicon.id', name='primer_amplicon_fk', ondelete='cascade'))
+    amplicon = relationship(
+        Amplicon,
+        backref=backref('primers', uselist=True, cascade='delete,all'))
+    name = Column(String(32), nullable=False, index=True)
+    description = Column(String(1024))
 
 
 class CellLine(Base):
@@ -67,52 +116,62 @@ class Clone(Base):
     control = Column(Boolean, nullable=False, default=False)
 
 
-class Experiment(Base):
-    __tablename__ = 'experiment'
-    id = Column(Integer, primary_key=True)
-    project_id = Column(Integer, ForeignKey('project.id', name='experiment_project_fk', ondelete='cascade'))
-    project = relationship(
-        Project,
-        backref=backref('experiments', uselist=True, cascade='delete,all'))
-    name = Column(String(32), index=True)
-    description = Column(String)
-
-
 class Plate(Base):
     __tablename__ = 'plate'
     id = Column(Integer, primary_key=True)
-    experiment_id = Column(Integer, ForeignKey('experiment.id', name='plate_experiment_fk', ondelete='cascade'))
-    experiment = relationship(
-        Experiment,
+    experiment_layout_id = Column(Integer, ForeignKey('experiment_layout.id', name='plate_experiment_layout_fk', ondelete='cascade'))
+    experiment_layout = relationship(
+        ExperimentLayout,
         backref=backref('plates', uselist=True, cascade='delete,all'))
-    name = Column(String(32), index=True)
+    barcode = Column(String(32), index=True)
     description = Column(String)
+
+
+guide_well_content_association = Table('guide_well_content_association', Base.metadata,
+    Column('well_content_id', Integer, ForeignKey('well_content.id')),
+    Column('guide_id', Integer, ForeignKey('guide.id'))
+)
+
+
+class WellContent(Base):
+    __tablename__ = 'well_content'
+    id = Column(Integer, primary_key=True) # is this id the replicate goup?
+    replicate_group = Column(Integer, nullable=False, default=0)
+    content_type = Column(Enum('wild_type', 'knockout', 'background', 'normaliser', 'empty', 'sample'), nullable=False)
+    clone_id = Column(Integer, ForeignKey('clone.id', name='well_content_clone_fk', ondelete='cascade'))
+    clone = relationship(
+        Clone,
+        backref=backref('well_contents', uselist=True, cascade='delete,all'))
+    guides = relationship('Guide', secondary=guide_well_content_association)
+
+
+class SequencingLibrary(Base):
+    __tablename__ = 'sequencing_library'
+    id = Column(Integer, primary_key=True)
+    slxid = Column(String(8), nullable=False)
+    library_type = Column(String(32))
+    barcode_size = Column(Integer)
 
 
 class Well(Base):
     __tablename__ = 'well'
     id = Column(Integer, primary_key=True)
-    plate_id = Column(Integer, ForeignKey('plate.id', name='well_plate_fk', ondelete='cascade'), nullable=False)
-    plate = relationship(
-        Plate,
+    experiment_layout_id = Column(Integer, ForeignKey('experiment_layout.id', name='well_experiment_layout_fk', ondelete='cascade'), nullable=False)
+    experiment_layout = relationship(
+        ExperimentLayout,
         backref=backref('wells', uselist=True, cascade='delete,all'))
-    clone_id = Column(Integer, ForeignKey('clone.id', name='well_clone_fk', ondelete='cascade'))
-    clone = relationship(
-        Clone,
+    well_content_id = Column(Integer, ForeignKey('well_content.id', name='well_well_content_fk', ondelete='cascade'), nullable=True)
+    well_content = relationship(
+        WellContent,
         backref=backref('wells', uselist=True, cascade='delete,all'))
-    target_id = Column(Integer, ForeignKey('target.id', name='well_target_fk', ondelete='cascade'))
-    target = relationship(
-        Target,
+    sequencing_library_id = Column(Integer, ForeignKey('sequencing_library.id', name='well_sequencing_library_fk', ondelete='cascade'), nullable=True)
+    sequencing_library = relationship(
+        SequencingLibrary,
         backref=backref('wells', uselist=True, cascade='delete,all'))
-    guide_id = Column(Integer, ForeignKey('guide.id', name='well_guide_fk', ondelete='cascade'))
-    guide = relationship(
-        Guide,
-        backref=backref('wells', uselist=True, cascade='delete,all'))
+    sequencing_barcode = Column(String(20))
     row = Column(String(1), nullable=False)
     column = Column(Integer, nullable=False)
-    replicate_group = Column(Integer, nullable=False, default=0)
-    well_type = Column(Enum('wild_type', 'knockout', 'background', 'normaliser', 'empty', 'sample'), nullable=False)
-    UniqueConstraint('plate_id', 'row', 'column', name='well_unique_in_plate')
+    UniqueConstraint('experiment_layout_id', 'row', 'column', name='well_unique_in_layout')
 
     def isEmpty(self):
         return self.clone == None
@@ -125,6 +184,10 @@ class ProteinAbundance(Base):
     well = relationship(
         Well,
         backref=backref('abundances', uselist=True, cascade='delete,all'))
+    plate_id = Column(Integer, ForeignKey('plate.id', name='abundance_plate_fk', ondelete='cascade'), nullable=False)
+    plate = relationship(
+        Plate,
+        backref=backref('abundances', uselist=True, cascade='delete,all'))
     intensity_channel_700 = Column(Float)
     intensity_channel_800 = Column(Float)
 
@@ -135,6 +198,10 @@ class CellGrowth(Base):
     well_id = Column(Integer, ForeignKey('well.id', name='growth_well_fk', ondelete='cascade'), nullable=False)
     well = relationship(
         Well,
+        backref=backref('growths', uselist=True, cascade='delete,all'))
+    plate_id = Column(Integer, ForeignKey('plate.id', name='abundance_plate_fk', ondelete='cascade'), nullable=False)
+    plate = relationship(
+        Plate,
         backref=backref('growths', uselist=True, cascade='delete,all'))
     timestamp = Column(DateTime, nullable=False)
     hours = Column(Integer, nullable=False)
