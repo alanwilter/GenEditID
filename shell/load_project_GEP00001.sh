8aa27237f80aab129d31b2d0980b7730cb8a9b0e
pajanne <pajanne@gmail.com>
2017-05-11 16:13:15 +0100

    fix plate id for loading protein abundance and cell growth

--- shell/load_project_GEP00001.sh
+++ shell/load_project_GEP00001.sh
@@ -14,8 +14,8 @@ python python/scripts/load_layout.py --layout=${PROJDIR}/${PROJDATE}_${PROJNAME}
 for plateid in 01 02 03 04 05 06
 do
   echo 'Loading data for plate' ${plateid}
-  python python/scripts/load_protein_abundance.py --plateid=${PROJNAME}_${plateid} --file=${PROJDIR}/${PROJNAME}_${plateid}_ICW.csv
-  python python/scripts/load_cell_growth.py --plateid=${PROJNAME}_${plateid} --file=${PROJDIR}/${PROJNAME}_${plateid}_incu.txt
+  python python/scripts/load_protein_abundance.py --plateid=${PROJNAME}_${plateid}_ICW --file=${PROJDIR}/${PROJNAME}_${plateid}_ICW.csv
+  python python/scripts/load_cell_growth.py --plateid=${PROJNAME}_${plateid}_incu --file=${PROJDIR}/${PROJNAME}_${plateid}_incu.txt
 done
 
 echo 'Loading sequencing variant results'
