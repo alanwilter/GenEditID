6e1b1ea7845f1f61e7b148fafe13f4f94e6f425f
Ruben Alvarez <alvare01@47lm95j.cri.camres.org>
2017-05-25 15:29:50 +0100

    updated layout_plot.R with DBI code fom layout_plot_DBI.R, and deleted the latter

--- r/scripts/layout_plot.R
+++ r/scripts/layout_plot.R
@@ -1,69 +1,72 @@
 library(dplyr)
 library(ggplot2)
 library(svglite)
-
-## Plot function, stolen from "PlateLayout".
+library(RPostgreSQL)
 
 create_classifier <- function(cellline, clone, guide, content)
 {
-    thing <- paste(cellline, clone, guide, content, sep=" ")
-    thing <- gsub(" NA", "", thing)
-    thing <- gsub(" sample", "", thing)
-    thing
+  thing <- paste(cellline, clone, guide, content, sep=" ")
+  thing <- gsub(" NA", "", thing)
+  thing <- gsub(" sample", "", thing)
+  thing <- gsub(" empty", "", thing)
+  thing
 }
 
-
-loadPlateData <- function(db, plateId)
+loadPlateData <- function(conn, layoutId)
 {
-    left_join(well, abundance, by="well_id") %>%
-    left_join(., well_content, by="well_content_id") %>%
-    left_join(., clone, by="clone_id") %>%
-    left_join(., cell_line, by="cell_line_id") %>%
-    left_join(., plate, by="plate_id") %>%
-    left_join(., guide_well_content_association, by="well_content_id") %>%
-    left_join(., guide, by="guide_id") %>%
-    left_join(., target, by="target_id") %>%
-    left_join(., sequencing_library_content, by="well_id") %>%
-    left_join(., variant_result, by="sequencing_library_content_id") %>%
-    collect %>%
-    filter(plate_geid == plateId) %>%
-    filter(!is.na(content_type)) %>%
-    mutate(classifier=create_classifier(cell_line_name, clone_name, guide_name, content_type)) %>%
-    select(classifier, row, column, ge_score) %>%
-    rename(Content=classifier, Row=row, Column=column, Score=ge_score)
+  sql <- paste0(
+    'select cl.name as cell_line_name, c.name as clone_name, t.name as target_name,
+    g.name as guide_name, wc.content_type, w.row, w.column, vr.ge_score
+    from well w
+    inner join experiment_layout el on w.experiment_layout_id = el.id
+    left join well_content wc on w.well_content_id = wc.id
+    left join sequencing_library_content slc on slc.well_id = w.id
+    left join variant_result vr on vr.sequencing_library_content_id = slc.id
+    left join clone c on wc.clone_id = c.id
+    left join cell_line cl on c.cell_line_id = cl.id
+    left join guide_well_content_association gwca on gwca.well_content_id = wc.id
+    inner join guide g on gwca.guide_id = g.id
+    inner join target t on g.target_id = t.id
+    where wc.content_type is not null
+    and el.geid = \'', layoutId, '\'')
+  
+  cat(sql)
+  cat('\n')
+  
+  query <- as.data.frame(dbGetQuery(conn, sql))
+  
+  query$classifier <- create_classifier(query$cell_line_name, query$clone_name, query$guide_name, query$content_type)
+  
+  print(query)
+  
+  query
 }
 
 
 ## Main code.
 
-db <- src_postgres(user="gene", password="gene", host="bioinf-ge001.cri.camres.org", port=5432, dbname="geneediting")
+driver <- dbDriver("PostgreSQL")
 
-cell_line <- tbl(db, "cell_line") %>% rename(cell_line_id=id, cell_line_name=name, cell_line_description=description)
-project <- tbl(db, "project") %>% rename(project_id=id, project_geid=geid, project_description=description)
-experiment_layout <- tbl(db, "experiment_layout") %>% rename(experiment_layout_id=id, experiment_layout_geid=geid)
-clone <- tbl(db, "clone") %>% rename(clone_id=id, clone_name=name)
-well <- tbl(db, "well") %>% rename(well_id=id)
-well_content <- tbl(db, "well_content") %>% rename(well_content_id=id)
-plate <- tbl(db, "plate") %>% rename(plate_id=id, plate_geid=geid)
-growth <- tbl(db, "growth") %>% rename(growth_id=id)
-abundance <- tbl(db, "abundance") %>% rename(abundance_id=id)
-target <- tbl(db, "target") %>% rename(target_id=id, target_name=name)
-guide <- tbl(db, "guide") %>% rename(guide_id=id, guide_name=name)
-guide_well_content_association <- tbl(db, "guide_well_content_association")
-sequencing_library_content <- tbl(db, "sequencing_library_content") %>% rename(sequencing_library_content_id=id)
-variant_result <- tbl(db, "variant_result") %>% rename(variant_result_id=id)
+conn <- dbConnect(driver, user="gene", password="gene", host="bioinf-ge001.cri.camres.org", port=5432, dbname="geneediting")
 
+layoutId <- 'GEP00001_03'
 
-## TODO: Pass plate identifier as argument to script.
-
-plateId <- 'GEP00001_01_ICW'
-
-plot.plate <- loadPlateData(db, plateId) %>%
-  mutate(Score = coalesce(Score, 0L)) %>% # replace NA's with 0's
-  plot_ly(x = ~Column, y = ~Row,
-        mode = "markers", type = "scatter", color = ~Score, colors = c('grey80', 'green4'),
-        marker = list(size = 40)
-        ) %>%
-  layout(yaxis = list(autorange = "reversed"))
-
-RPostgreSQL::dbDisconnect(db$con)
+tryCatch(
+  {
+    plateData <- loadPlateData(conn, layoutId)
+    plateData %>%
+      as.data.frame %>%
+      mutate(ge_score = coalesce(ge_score, 0L)) %>% # replace NA's with 0's
+      plot_ly(x = ~column, y = ~row,
+              mode = "markers", type = "scatter", color = ~ge_score, colors = c('grey90', 'orange', 'green4'),
+              marker = list(size = 40),
+              # Hover text:
+              text = ~paste0(row, column, ' score: ', ge_score)
+      ) %>%
+      layout(yaxis = list(autorange = "reversed"))
+  },
+  finally =
+  {
+    dbDisconnect(conn)
+  }
+)
