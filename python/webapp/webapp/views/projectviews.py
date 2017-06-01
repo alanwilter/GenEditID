4d9d05abacbd61163dc0bd3e639af9eb6a2b8ca5
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2017-06-01 16:10:10 +0100

    Got edit form working.

--- python/webapp/webapp/views/projectviews.py
+++ python/webapp/webapp/views/projectviews.py
@@ -10,11 +10,10 @@ from dnascissors.model import Project
 
 # File uploads: http://docs.pylonsproject.org/projects/pyramid-cookbook/en/latest/forms/file_uploads.html
 
-'''
-class SelectContent(colander.MappingSchema):
-    id = colandar.SchemaNode(colander.Integer())
-    name = colandar.SchemaNode(colander.String())
-'''
+class ProjectContent(colander.MappingSchema):
+    # id = colander.SchemaNode(colander.Integer())
+    geid = colander.SchemaNode(colander.String(), title="GEID")
+    name = colander.SchemaNode(colander.String(), title="Name")
 
 class ProjectViews(object):
     
@@ -24,7 +23,7 @@ class ProjectViews(object):
     
     @property
     def projects_form(self):
-        schema = SelectContent()
+        schema = ProjectContent().bind(request=self.request)
         return deform.Form(schema, buttons=('submit',))
 
     @view_config(route_name="projects", renderer="../templates/selectproject.pt")
@@ -37,6 +36,36 @@ class ProjectViews(object):
         project = self.dbsession.query(Project).filter(Project.id == id).first()
         return dict(project=project, title="Gene Editing Project %s" % project.geid)
     
+    @view_config(route_name="project_edit", renderer="../templates/editproject.pt")
+    def edit_project(self):
+        id = self.request.matchdict['projectid']
+        project = self.dbsession.query(Project).filter(Project.id == id).first()
+        
+        edit_form = self.projects_form
+        
+        if 'submit' in self.request.params:
+            
+            fields = self.request.POST.items()
+            
+            try:
+                appstruct = projects_form.validate(fields)
+            except deform.ValidationFailure as e:
+                return dict(project=project, form=e.render())
+            
+            #project.geid = appstruct['geid']
+            #project.name = appstruct['name']
+            
+            url = self.request.route_url('project_view', projectid=project.id)
+            return HTTPFound(url)
+        
+        projectMap = dict(id=project.id, geid=project.geid, name=project.name)
+        
+        form = edit_form.render(projectMap)
+        
+        return dict(edit_form=form, projectid=project.id,
+                    project=project, title="Gene Editing Project %s" % project.geid)
+
+        
 '''    
     @view_config(route_name="projects", renderer="../templates/selectproject.pt")
     def view_projects(self):
