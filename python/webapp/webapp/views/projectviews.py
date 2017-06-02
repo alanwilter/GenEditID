a9b6e9da9e4ffb1ec45edae46fd39e882b8bf2c9
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2017-06-02 14:35:45 +0100

    Created "add" page.

--- python/webapp/webapp/views/projectviews.py
+++ python/webapp/webapp/views/projectviews.py
@@ -1,5 +1,6 @@
 import colander
 import deform.widget
+import datetime
 
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
@@ -36,6 +37,38 @@ class ProjectViews(object):
         project = self.dbsession.query(Project).filter(Project.id == id).first()
         return dict(project=project, title="Gene Editing Project %s" % project.geid)
     
+    @view_config(route_name="project_add", renderer="../templates/addproject.pt")
+    def add_project(self):
+        title = "Create New Gene Editing Project"
+        
+        add_form = self.projects_form
+        
+        if 'submit' in self.request.params:
+            
+            fields = self.request.POST.items()
+            
+            try:
+                appstruct = add_form.validate(fields)
+            except deform.ValidationFailure as e:
+                return dict(project=project, form=e.render(), title=title)
+            
+            print("New id = %s" % appstruct['geid'])
+            print("New name = %s" % appstruct['name'])
+            
+            project = Project()
+            project.geid = appstruct['geid']
+            project.name = appstruct['name']
+            project.start_date = datetime.datetime.date(datetime.datetime.now())
+            
+            self.dbsession.add(project)
+            
+            url = self.request.route_url('projects')
+            return HTTPFound(url)
+        
+        form = add_form.render(dict())
+        
+        return dict(add_form=form, title=title)
+
     @view_config(route_name="project_edit", renderer="../templates/editproject.pt")
     def edit_project(self):
         id = self.request.matchdict['projectid']
