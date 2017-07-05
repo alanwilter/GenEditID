25ae2ddc11a33737be1a01e1fc9ddb6858aad9e7
Ruben Alvarez <alvare01@47lm95j.cri.camres.org>
2017-07-05 14:43:47 +0100

    zygosities bar plot finished. It works for any number of guides, but PROJECT is still hardcoded

--- python/scripts/plot_zygosities.py
+++ python/scripts/plot_zygosities.py
@@ -3,10 +3,15 @@
 Created on Wed Jun 28 14:27:18 2017
 
 @author: alvare01
+
+plot description: 
+Query database to get guides and zygosities ('wt', 'homo', 'dmut'...).
+Calculation of percentages of zygosities ('wt', 'homo', 'dmut'...) on a per-guide basis
+Bar plot
+
 """
 
 import sqlalchemy
-import plotly
 import plotly.graph_objs as go
 import plotly.offline as pyoff
 
@@ -17,8 +22,10 @@ from dnascissors.model import SequencingLibraryContent
 from dnascissors.model import Well
 from dnascissors.model import ExperimentLayout
 from dnascissors.model import Project
-from collections import Counter
+from pandas import DataFrame
+from pandas import groupby
 
+#-------- sqlalchemy query
 engine = sqlalchemy.create_engine(cfg['DATABASE_URI'])
 Base.metadata.bind = engine
 DBSession = sqlalchemy.orm.sessionmaker(bind=engine)
@@ -33,11 +40,9 @@ results = session.query(SequencingLibraryContent)\
                  .filter(Project.geid == 'GEP00001')\
                  .filter(SequencingLibraryContent.dna_source != 'gDNA')\
                  .all()
- 
-data = []
-                
+
+guide = []
 zygosities = []
-            
 for i in results:
     well = i.well
     layout = well.experiment_layout
@@ -50,31 +55,46 @@ for i in results:
         guide_name = well.well_content.guides[0].name
     print(i.sequencing_sample_name, guide_name, mutation_zygosity)
     zygosities.append(mutation_zygosity)
+    guide.append(guide_name)
     
 print(len(results))
 
-# percentajes
-zygosities_counter = Counter(zygosities)
-zygosities_counts = zygosities_counter.values()
-zygosities_total = len(zygosities)
-zygosities_percent = {key: round((count/zygosities_total)*100,2) for key, count in zygosities_counter.items()}
+#-------- calculate percentages per guide in a pandas dataframe
+# convert 'results' to pandas dataframe and group by 'guides'
+df = DataFrame({'guides': guide, 'zygosities': zygosities})
+dfgroup = df.groupby(['guides'])
 
+#for guidename, grouped_data in dfgroup:
+#    print(guidename)
+#    grouped_data_byzygosity = grouped_data.groupby(['zygosities']).size()
+#    grouped_data_byzygosity_percent = grouped_data_byzygosity*100 / grouped_data_byzygosity.sum()
+#    print(grouped_data_byzygosity_percent)
+#    
+#for guidename in dfgroup:
+#    grouped_data_byzygosity = grouped_data.groupby(['zygosities']).size()
+#    grouped_data_byzygosity_percent = grouped_data_byzygosity*100 / grouped_data_byzygosity.sum()
 
-#------ plots
-trace = go.Bar(
-    x = list(zygosities_percent.keys()),
-    y = list(zygosities_percent.values())
-)
+# calculate percentages of zygosities and create bar plot 'data' dictionary
+data = []
+for guidename, grouped_data in dfgroup:
+    grouped_data_byzygosity = grouped_data.groupby(['zygosities']).size()
+    grouped_data_byzygosity_percent = grouped_data_byzygosity*100 / grouped_data_byzygosity.sum()
+    data.append(
+        go.Bar(
+             x = grouped_data_byzygosity_percent.index.tolist(),
+             y = grouped_data_byzygosity_percent.tolist(),
+             name = guidename
+             )
+    )
+    
 
-# ordered values for categoric x axis
+# order x axis values
 vals = ['wt', 'homo', 'smut', 'dmut', 'iffy']
-
 layout = go.Layout(
     title = 'Zygosities',
     xaxis = {'categoryorder': 'array', 'categoryarray': vals},
     yaxis = {'title': '% of submitted samples'}    
 )
 
-pyoff.plot(
-dict(data = [trace], layout = layout)
-)
+# plot
+pyoff.plot(dict(data = data, layout = layout))
