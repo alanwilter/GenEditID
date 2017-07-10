64352a29173bd79314b3cff5247b4264d8da859b
Ruben Alvarez <alvare01@47lm95j.cri.camres.org>
2017-07-10 18:55:50 +0100

    the plots show all samples now, but query must be revised as per gitter message

--- python/scripts/plot_distances.py
+++ python/scripts/plot_distances.py
@@ -12,10 +12,11 @@ Bar plot
 """
 
 import sqlalchemy
+import pandas
 import plotly.graph_objs as go
 import plotly.offline as pyoff
 
-# from dnascissors.model import MutationSummary
+from dnascissors.config import cfg
 from dnascissors.model import Base
 from dnascissors.model import SequencingLibraryContent
 from dnascissors.model import Well
@@ -23,65 +24,63 @@ from dnascissors.model import ExperimentLayout
 from dnascissors.model import Project
 from dnascissors.model import VariantResult
 
-from dnascissors.config import cfg
-
-from pandas import DataFrame
-from pandas import groupby
-
 #-------- sqlalchemy query
 engine = sqlalchemy.create_engine(cfg['DATABASE_URI'])
 Base.metadata.bind = engine
 DBSession = sqlalchemy.orm.sessionmaker(bind=engine)
 session = DBSession()
 
-results = session.query(SequencingLibraryContent)\
-                 .outerjoin(SequencingLibraryContent.variant_results)\
+results = session.query(VariantResult)\
+                 .join(VariantResult.sequencing_library_content)\
                  .join(SequencingLibraryContent.well)\
                  .join(Well.well_content)\
                  .join(Well.experiment_layout)\
                  .join(ExperimentLayout.project)\
                  .filter(Project.geid == 'GEP00001')\
                  .filter(SequencingLibraryContent.dna_source != 'gDNA')\
+                 .filter(VariantResult.allele_fraction > 0.1)\
                  .all()
                  
 guide = []
 allindellengths = []
+typecaller = []
+
 for i in results:
-    well = i.well
+    well = i.sequencing_library_content.well
     layout = well.experiment_layout
     indellength = 0
     guide_name = 'none'
     
-    if i.variant_results:
-        indellength = i.variant_results[0].indel_length
     if well.well_content.guides: 
         guide_name = well.well_content.guides[0].name
-    print(i.sequencing_sample_name, guide_name, indellength)
-    allindellengths.append(indellength)
-    guide.append(guide_name)
+    print(i.sequencing_library_content.sequencing_sample_name, guide_name, i.indel_length, i.variant_caller)
+    guide.append(guide_name)    
+    allindellengths.append(i.indel_length)
+    typecaller.append(i.variant_caller)
 
 print(len(results))
 
+
 #-------- calculate percentages per guide in a pandas dataframe
 # convert 'results' to pandas dataframe and group by 'guides'
-df = DataFrame({'guides': guide, 'indellengths': allindellengths}) ##PROBLEM HERE! I expect a higher number of lenghts (this is per allele), and don't know why I'm getting NaN's
-dfgroup = df.groupby(['guides'])
+df = pandas.DataFrame({'caller': typecaller, 'guides': guide, 'indellengths': allindellengths})
 
 # calculate percentages of indel lengths and create bar plot 'data' dictionary
 data = []
-for guidename, grouped_data in dfgroup:
-    grouped_data_byvar = grouped_data.groupby(['indellengths']).size()
-    grouped_data_byvar_percent = grouped_data_byvar*100 / grouped_data_byvar.sum()
-    data.append(
-        go.Bar(
-             x = grouped_data_byvar_percent.index.tolist(),
-             y = grouped_data_byvar_percent.tolist(),
-             name = guidename
-             )
-    )
-    
+for caller, gdata in df.groupby(['caller']):
+    for guidename, gdata2 in gdata.groupby(['guides']):
+        gdata2_byvar = gdata.groupby(['indellengths']).size()
+        gdata2_byvar_percent = gdata2_byvar*100 / gdata2_byvar.sum()
+        data.append(
+            go.Bar(
+            x = gdata2_byvar_percent.index.tolist(),
+            y = gdata2_byvar_percent.tolist(),
+            legendgroup = caller,
+            name = guidename
+            )
+        )
 
-# order x axis values
+
 layout = go.Layout(
     title = 'Indel lengths',
     yaxis = {'title': '% of submitted samples per guide'}    
@@ -89,3 +88,4 @@ layout = go.Layout(
 
 # plot
 pyoff.plot(dict(data = data, layout = layout))
+
---
+++ python/scripts/plot_typeofvariant.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 15:34:57 2017
+
+@author: alvare01
+
+plot description: 
+Query database to get guides and type of mutation ('wt', 'frameshift', 'inframe deletion'...).
+Calculation of percentages of types of mutation on a per-guide and per-allele basis
+Bar plot
+
+"""
+
+import sqlalchemy
+import pandas
+import plotly.graph_objs as go
+import plotly.offline as pyoff
+
+from dnascissors.config import cfg
+from dnascissors.model import Base
+from dnascissors.model import SequencingLibraryContent
+from dnascissors.model import Well
+from dnascissors.model import ExperimentLayout
+from dnascissors.model import Project
+from dnascissors.model import VariantResult
+
+
+# from plotly import tools. Possible use to make subplots
+
+#-------- sqlalchemy query
+
+engine = sqlalchemy.create_engine(cfg['DATABASE_URI'])
+Base.metadata.bind = engine
+DBSession = sqlalchemy.orm.sessionmaker(bind=engine)
+session = DBSession()
+
+results = session.query(VariantResult)\
+                 .join(VariantResult.sequencing_library_content)\
+                 .join(SequencingLibraryContent.well)\
+                 .join(Well.well_content)\
+                 .join(Well.experiment_layout)\
+                 .join(ExperimentLayout.project)\
+                 .filter(Project.geid == 'GEP00001')\
+                 .filter(SequencingLibraryContent.dna_source != 'gDNA')\
+                 .filter(VariantResult.allele_fraction > 0.1)\
+                 .all()
+
+guide = []
+typemutation = []
+typevariant = []
+typecaller = []
+
+for i in results:
+    well = i.sequencing_library_content.well
+    layout = well.experiment_layout
+    typemut = 'wt'
+    guide_name = 'none'
+    
+    if well.well_content.guides:
+        guide_name = well.well_content.guides[0].name
+    print(guide_name, i.consequence, i.variant_type, i.variant_caller)
+    
+    guide.append(guide_name)
+    typemutation.append(i.consequence)
+    typevariant.append(i.variant_type)
+    typecaller.append(i.variant_caller)
+
+print(len(results))
+
+
+#-------- calculate percentages per guide in a pandas dataframe
+# convert 'results' to pandas dataframe and group by 'variants'
+df = pandas.DataFrame({'variants': typevariant, 'caller': typecaller, 'guides': guide, 'mutation': typemutation})
+dfgroup_typevariant = df.groupby(['variants'])
+
+# create independent datasets (one for indels, one for snvs) for plotting
+dfgroup_typevariant_INDEL = dfgroup_typevariant.get_group('INDEL')  #[i for i in dfgroup_typevariant][0][1]
+dfgroup_typevariant_SNV = dfgroup_typevariant.get_group('SNV')      #[i for i in dfgroup_typevariant][1][1]
+
+# calculate percentages of mutation types and create bar plot 'data' dictionary
+# This will produce a plot with grouped legends. Annoyingly there no feature at date 20170710 to add 
+#  titles to the legend groups.
+#  It's been suggested to use layout annotations as a workaround: https://github.com/plotly/plotly.js/issues/689
+#  I assume that the top legend group is the first variant caller.
+data_INDELs = []
+for caller, gdata in dfgroup_typevariant_INDEL.groupby(['caller']):
+    for guidename, gdata2 in gdata.groupby(['guides']):
+#        print(caller)
+#        print(guidename)
+#        print(gdata2)
+        gdata2_byvar = gdata2.groupby(['mutation']).size()
+        gdata2_byvar_percent = gdata2_byvar*100 / gdata2_byvar.sum()
+        data_INDELs.append(
+            go.Bar(
+                 x = gdata2_byvar_percent.index.tolist(),
+                 y = gdata2_byvar_percent.tolist(),
+                 legendgroup = caller,
+                 name = guidename
+                 )
+        ) 
+
+data_SNVs = []
+for caller, gdata in dfgroup_typevariant_SNV.groupby(['caller']):
+    for guidename, gdata2 in gdata.groupby(['guides']):
+#        print(caller)
+#        print(guidename)
+#        print(gdata2)
+        gdata2_byvar = gdata2.groupby(['mutation']).size()
+        gdata2_byvar_percent = gdata2_byvar*100 / gdata2_byvar.sum()
+        data_SNVs.append(
+            go.Bar(
+                 x = gdata2_byvar_percent.index.tolist(),
+                 y = gdata2_byvar_percent.tolist(),
+                 legendgroup = caller,
+                 name = guidename
+                 )
+        ) 
+
+ 
+# layouts
+layout_INDELs = go.Layout(
+    title = 'Type of mutation (INDELS)',
+    yaxis = {'title': '% of submitted samples per guide'}    
+)
+
+layout_SNVs = go.Layout(
+    title = 'Type of mutation (SNVs)',
+    yaxis = {'title': '% of submitted samples per guide'}    
+)
+
+# plot
+pyoff.plot(dict(data = data_INDELs, layout = layout_INDELs))
+pyoff.plot(dict(data = data_SNVs, layout = layout_SNVs))
+    # not sure why, but when the script is sourced, both plots are the SNV plot
+
+
+# the ideal situation would be to plot both datasets side by side
+ # pyoff.plot(dict(data = data_INDELs + data_SNVs, layout = layout_INDELs)) #say we use a different layout for each dataset
+ # however the guides appear duplicated. As far as I have seen, you can't have 
+ # the same legent for two different plots in plotly (ironically you can have it in if you use ggplotly(R:ggplot2)... )
