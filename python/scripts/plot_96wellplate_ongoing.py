3945fd0185d85e548310f6e3ba2187025e08d447
Ruben Alvarez <alvare01@47lm95j.cri.camres.org>
2017-07-19 17:05:18 +0100

    plot 96 well plates finished

--- python/scripts/plot_96wellplate_ongoing.py
+++ python/scripts/plot_96wellplate_ongoing.py
@@ -6,7 +6,8 @@ Created on Tue Jul 11 12:32:07 2017
 
 plot description: 
 Query database to get sample names, plate, well locations, guides, scores, protein value, slope value and zygosity per clone
-Create a scatter plot with the shape of a 96-well plate, mapping the scores to a colour gradient
+Create a scatter plot with the shape of a 96-well plate, mapping the scores to 
+    a colour gradient and showing hover data: guide, zygosity, protein ratio and score
 
 """
 
@@ -17,11 +18,9 @@ import plotly.offline as pyoff
 
 from plotly import tools
 from math import ceil, floor
-from itertools import cycle
 
 from dnascissors.config import cfg
 from dnascissors.model import Base
-from dnascissors.model import SequencingLibraryContent
 from dnascissors.model import Well
 from dnascissors.model import ExperimentLayout
 from dnascissors.model import Project
@@ -34,203 +33,137 @@ session = DBSession()
 
 # sqlalchemy query
 results = session.query(Well)\
-                 .join(Well.well_content)\
                  .join(Well.experiment_layout)\
                  .join(ExperimentLayout.project)\
-                 .outerjoin(SequencingLibraryContent.variant_results)\
-                 .outerjoin(SequencingLibraryContent.sequencing_sample_name)\
                  .filter(Project.geid == 'GEP00001')\
-                 .filter(SequencingLibraryContent.dna_source != 'gDNA')\
                  .all()
-                
-#                query(SequencingLibraryContent)\
-#                 .outerjoin(SequencingLibraryContent.mutation_summaries)\
-#                 .join(SequencingLibraryContent.well)\
-#                 .join(Well.well_content)\
-#                 .join(Well.experiment_layout)\
-#                 .join(ExperimentLayout.project)\
-#                 .filter(Project.geid == 'GEP00001')\
-#                 .filter(SequencingLibraryContent.dna_source != 'gDNA')\
-#                 .all()
-    
-    
-    
-df = []
-zygosities = []
-for i in results:
-    mut_zygosities = 'wt'
-    guide_name = 'none'
-    well = i.well
-    row = [i.sequencing_sample_name,
-           i.well.experiment_layout.geid,
+
+# data collection
+data = []
+for i, well in enumerate(results, start=1):
+    zygosity = 'empty-well'
+    guide_name = 'no-guide'
+    sequencing_sample_name = 'not-sequenced'
+    dna_source = 'not-sequenced'
+    well_content_type = 'empty-well'
+    score = 0
+    row = [well.experiment_layout.project.geid,
            well.row,
            well.column,
-           ':'.join([g.name for g in well.well_content.guides]),
-            well.abundances[0].intensity_channel_800/well.abundances[0].intensity_channel_700
-           #[a.intensity_channel_800/a.intensity_channel_700 for a in well.abundances],
-           #[(g.hours, g.confluence_percentage) for g in well.growths],
+           well.abundances[0].ratio_800_700
            ]
-    if i.mutation_summaries:
-        mut_zygosities = i.mutation_summaries[0].zygosity
-    rowzygo = row + [mut_zygosities]
-    df.append(rowzygo)
+    if well.well_content:
+        well_content_type = 'sample-well'
+        zygosity = 'wt'
+        if well.well_content.guides:
+            guide_name = well.well_content.guides[0].name
+    if well.sequencing_library_contents:
+        sequencing_sample_name = well.sequencing_library_contents[0].sequencing_sample_name
+        if well.sequencing_library_contents[0].mutation_summaries:
+            dna_source = well.sequencing_library_contents[0].dna_source
+            score = well.sequencing_library_contents[0].mutation_summaries[0].score
+            # sequencing_library_contents[0].dna_source corresponds to fixed cells,
+            # and sequencing_library_contents[1].dna_source to gDNA
+            zygosity = well.sequencing_library_contents[0].mutation_summaries[0].zygosity
+    data.append([
+                well.experiment_layout.project.geid,
+                well.experiment_layout.geid,
+                well.row,
+                well.column,
+                guide_name,
+                well.abundances[0].ratio_800_700,
+                zygosity,
+                score,
+                well_content_type
+                ])
 
-# convert to pandas dataframe
-df = pandas.DataFrame(df)
-# column_names = ['sample', 'plate', 'row', 'column', 'guide', 'protein_abundance', 'zygosity']
-dfgroup = df.groupby([1])
+# convert to pandas dataframe and add column names
+df = pandas.DataFrame(data)
+column_names = ['project', 'plate', 'row', 'column', 'guide', 'protein_abundance', 'zygosity', 'score', 'well_content_type']
+df = df.rename(columns = dict(enumerate(column_names)))
 
+# group by plate layout
+dfgroup = df.groupby(['plate'])
 
+## plot
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# plot
-data = []
+ # custom colorscale for the score color bar. The scale goes from 0 to 1
+ # (0% to 100% of values in the plot)
+color_scale=[[0.0, 'rgb(239, 239, 239)'], # e.g. this color is applied to the first 10% of values
+            [0.3, 'rgb(200, 223, 247)'],
+            [0.5, 'rgb(237, 119, 90)'],
+            [0.6, 'rgb(237, 195, 90)'],
+            [0.7, 'rgb((237, 229, 90)'],
+            [0.8, 'rgb((181, 221, 24)'],
+            [1, 'rgb(39, 132, 1)']]
+            
+ # construct the data dictionary            
+dataplot = []
+nloop = 0
 for i, grouped_data in dfgroup:
-    xnull = None # apparently I have to have values in all x and y coordinates so the plot axes show all coordinates (1:12 and A:H)
-    ynull = None
-    scatter =   go.Scatter(
-                    x=grouped_data[2] if grouped_data[2] else x = xnyll,
-                    y=grouped_data[3] if grouped_data[3],
-                    name=i,
-                    mode = 'markers',
-                    marker = dict(
-                        size = '20',
-                        color = df[df[4] == 'STAT3.2'][5],
-                        colorscale='Viridis',
-                        showscale=True
-                        )
-                    )
-    data.append(scatter)
-
-
-#trace0 = go.Scatter(
-#    x = df[df[4] == 'STAT3.2'][2],
-#    y = df[df[4] == 'STAT3.2'][3],
-#    mode = 'markers',
-#    marker = dict(
-#        size = '20',
-#        color = df[df[4] == 'STAT3.2'][5],
-#        colorscale='Viridis',
-#        showscale=True
-#        )
-#    )
-
-#To remove a trace's colorbar use the showscale41 attribute.
-#
-#in your case, you could remove all but the first trace's colorbar and make that colorbar span [0, 50] using cmin12 and cmax3.
-    
-#layout = go.Layout(
-#    autosize=False,
-#    width=400,
-#    height=230,
-#    margin=go.Margin(
-#        l=15,
-#        r=15,
-#        b=15,
-#        t=15
-#    )
-#)
-
+    nloop += 1
+    # create hover text to add to the scatter data structure
+    hovertext = []
+    for index, rows in grouped_data.iterrows():
+        htext = [rows.guide, rows.zygosity, 'protein=' + str(round(rows.protein_abundance, 2)), 'score=' + str(rows.score)]
+        htext = ', '.join(htext) # the hover text needs to be a single string, it can't be a list
+        hovertext.append(htext)
+    # create scatter plot data structure
+    scatter = go.Scatter(
+        x = grouped_data['column'],
+        y = grouped_data['row'],
+        name = i,        # i is the plate
+        mode = 'markers',
+        marker = dict(
+            size = '20',                   # dot size
+            color = grouped_data['score'], # assign a color based on score
+            showscale = True if nloop == 1 else False,
+            colorscale = color_scale,
+            cmin = 7000, # min value of the colorscale
+            cmax = 9000, # max value of the colorscale
+            colorbar = { # side color bar custom size, fraction of plot size (otherwise it takes all plot height)
+                'lenmode':"fraction",
+                'len':0.4                
+            }
+        ),
+        # hover text
+        text = hovertext
+    )
+    dataplot.append(scatter)
 
 # create plot layout in a grid of two columns and n rows
  # calculate number of subplots (number of plates)
-numberofplates = len(set(df[1]))
+numberofplates = len(set(df['plate']))
 numberofplotrows = ceil(numberofplates / 2)
+plotheight = numberofplotrows*330 # calculation of total plot height, see comment further down
 
 # create figure
-fig = tools.make_subplots(rows=numberofplotrows, cols=2)
+subplottitles = [i for i,j in dfgroup]
+fig = tools.make_subplots(rows = numberofplotrows, cols = 2, subplot_titles = subplottitles)
 row_counter = 1
-for i,j in zip(range(0, numberofplates), data):
+for i,j in zip(range(0, numberofplates), dataplot):
     subplot_row    = floor(row_counter)  # the roughest way to change row every two loops? Oh dear.
     row_counter    = row_counter + 0.5
     subplot_column = 1 if 1&i == 0 else 2
     print(row_counter, subplot_row, subplot_column)
     fig.append_trace(j, subplot_row, subplot_column)
 
-# layout construction. In the scatter subplots, each plot has an independent axis 
+# update layout construction. In the scatter subplots, each plot has an independent axis 
   # (xaxis, xaxis2... xaxisN, and the same for yaxis). Each layout for each subplot
   # must be built independently.
 
 for i in fig.layout:
-    fig.layout[i].update({'type': 'category', 'autoscale':'False'})
     if i.find('xaxis') == 0:
-        fig.layout[i].update({'categoryorder': 'array', 'categoryarray': list(range(1,13))})
+        fig.layout[i].update({'categoryorder': 'array', 'categoryarray': list(range(1,13))}) # this is to show all xaxis values in the plot, in this order
+        fig.layout[i].update({'type': 'category'})
     elif i.find('yaxis') == 0:
-        fig.layout[i].update({'categoryorder': 'array', 'categoryarray': ['A', 'B', 'C', 'D', 'E', 'F', 'G']})
+        fig.layout[i].update({'categoryorder': 'array', 'categoryarray': ['H', 'G', 'F', 'E', 'D', 'C', 'B', 'A']}) # this is to show all yaxis values in the plot, in this order
+        fig.layout[i].update({'type': 'category'})
+        fig.layout[i].update({'type': 'category'})
 
-pyoff.plot(fig)
+# with this layout.update below, width and height are set in the plot.Perhaps you can set them directly on the plotting area on the web page
+    # hovermode = closest shows the values for the hover point, otherwise by default ('compare' mode) you only see one coordinate
+fig.layout.update(dict(autosize = False, width = 850, height = plotheight, hovermode = 'closest'))
 
-
-
-
-layout = go.Layout(
-    xaxis = dict(range = list(range(1,13))),
-    yaxis = dict(range = [['A', 'B', 'C', 'D', 'E', 'F', 'G']])
-)
-
-#the idea is to loop through the keys in the layout, and if 'xaxis' is found add {'range': list(range(1,13))},
-# and if 'yaxis' is found add {'range': ['A', 'B', 'C', 'D', 'E', 'F', 'G']} using the .update method.
-# example a.update({'range':['A', 'B', 'C']})
-for i in fig.layout:
-    for n in i.keys():
-        n.find('xaxis')
- 
-
-   
-fig['layout'].update(
-    xaxis = dict(range = list(range(1,13))),
-    yaxis = dict(range = [['A', 'B', 'C', 'D', 'E', 'F', 'G']])
-    )
-
-#fig.append_trace(data[0], 1, 1)
-#fig.append_trace(data[1], 1, 2)
-#fig.append_trace(data[2], 2, 1)
-#fig.append_trace(data[3], 2, 2)
-pyoff.plot(fig)
-
-
-
-
-
-
-
-plotlayout = []
-for i in range(numberofplates):
-    layout = go.Layout(        
-        xaxis = {'domain': [0, subplot_xposition]},
-        yaxis = {'domain': [0, subplot_xposition]}
-        )
-    plotlayout.append(layout)
-
-fig = tools.make_subplots(rows=1, cols=2)
-
-fig.append_trace(trace1, 1, 1)
-fig.append_trace(trace2, 1, 2)
-    
-    
-layout = []
-
-
-
-layout = go.Layout(
-    xaxis=dict(
-        domain=[0, 0.45 * 1&i]
-    ),
-    yaxis=dict(
-        domain=[0, 0.45]
-
+# plot
+pyoff.plot(fig, filename = 'plot96.html')
