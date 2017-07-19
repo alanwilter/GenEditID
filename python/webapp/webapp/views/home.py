0514823a97a84909fbdc8108c7ff62f12e9d3ad6
pajanne <pajanne@gmail.com>
2017-07-19 23:32:46 +0100

    aesthetic modif

--- python/webapp/webapp/views/home.py
+++ python/webapp/webapp/views/home.py
@@ -77,7 +77,7 @@ class HomeViews(object):
                         url = self.request.route_url('home')
                         return HTTPFound(url)
                     except ExistingEntityException as e:
-                        return_map['error'] = e.message
+                        #return_map['error'] = e.message
                         return_map['clash'] = e
                         return_map['layoutfile'] = self.request.POST['layoutfile']
                 except Exception as e:
