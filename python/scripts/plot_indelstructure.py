953ff871a7ac3bb821465555091680bf755f2e34
Ruben Alvarez <alvare01@47lm95j.cri.camres.org>
2017-08-07 14:29:46 +0100

    pandas-free script!

--- python/scripts/plot_indelstructure.py
+++ python/scripts/plot_indelstructure.py
@@ -5,10 +5,8 @@ Created on Thu Aug  3 16:13:21 2017
 @author: alvare01
 """
 
-import pandas
 import sqlalchemy
 
-import plotly.graph_objs as go
 import plotly.offline as py
 from plotly import tools
 
@@ -42,15 +40,33 @@ query = dbsession.query(VariantResult)\
 results = query.all()
 # notes: this query gets only wells with guides, not gDNA, allele fraction > 0.1 and no offtargets
 
+# get the caller and sequencing_sample_names
+results_callers = list(set(i.variant_caller for i in query.all()))
+results_samples = list(set(i.sequencing_library_content.sequencing_sample_name for i in query.all()))
+# initialise the plot dictionaries
+plotdict = {}
+for caller in results_callers:
+    plotcaller = {}
+    for sam in results_samples:
+        plotcaller.update({sam:[]})
+    plotdict.update({caller:plotcaller})
+
+# populate the plot dictionaries on the fly from the database
 data =[]
 guidedict = {} #list of guides with their coordinates
+guidename_log = set()
+plotdata_xaxisrange = []
+shapecolor = {'SNV': 'rgba(0, 0, 0, 0.5)', 'insertion': 'rgba(239, 163, 64, 0.5)', 'deletion': 'rgba(112, 161, 239, 0.5)'}
+shapetype = {'SNV': 'circle', 'insertion': 'rect', 'deletion': 'rect'}
 for i in results:
     well = i.sequencing_library_content.well
     variant_caller = i.variant_caller
+    seqsample = i.sequencing_library_content.sequencing_sample_name
     plate = well.experiment_layout.geid
     well_position = "{:s}{:02}".format(well.row, well.column)
     guide_name = well.well_content.guides[0].name
-    guide_coordinate = well.well_content.guides[0].amplicon_selections[0].guide_location
+    guide_coordinate = well.well_content.guides[0].amplicon_selections[0].guide_location % 10000 #to get just the last 4 digits, eg. 12345676 > 5676
+    xaxisrange = [guide_coordinate - 125, guide_coordinate +125]
     mutation_length = 0 if i.indel_length == None else i.indel_length
     if mutation_length ==0:
         mutation = 'SNV'
@@ -60,69 +76,37 @@ for i in results:
         mutation = 'deletion'
     mutation_coordinate = i.position
     mutation_coordinate_start = mutation_coordinate if mutation_length >=0 else mutation_coordinate - abs(mutation_length)
+    mutation_coordinate_start =mutation_coordinate_start % 10000
     mutation_coordinate_end = mutation_coordinate + mutation_length if mutation_length >=0 else mutation_coordinate
+    mutation_coordinate_end = mutation_coordinate_end % 10000
     allele_fraction = i.allele_fraction
-    data.append([
-        variant_caller,
-        plate,
-        well_position,
-        guide_name,
-        guide_coordinate % 10000, #to get just the last 4 digits, eg. 12345676 > 5676
-        mutation,
-        mutation_coordinate_start % 10000,
-        mutation_coordinate_end % 10000,
-        allele_fraction,
-        mutation_length
-    ])
-    guidedict.update({guide_name:guide_coordinate % 10000})
+    hovertext = [', '.join([variant_caller,'well: ' + '-'.join([plate, well_position]),
+                            'length: '+ str(mutation_length), guide_name])] * 2
+    guidedict.update({guide_name:guide_coordinate})
+    plotdict[variant_caller][seqsample].append({
+        'x':[mutation_coordinate_start, mutation_coordinate_end],
+        'y':[allele_fraction]*2,
+        'name':guide_name,
+        'mode':'lines',
+        'legendgroup':guide_name,
+        'showlegend':False if guide_name in guidename_log else True,
+        'line':{
+            'color':shapecolor.get(mutation),
+            'width':5
+        },
+        'text': hovertext  
+    }) 
+    guidename_log.update([guide_name])
+    plotdata_xaxisrange.append(xaxisrange)
+    
 
-df = pandas.DataFrame(data)
-column_names = ['caller', 'plate', 'well_position', 'guide', 'guide_coordinate',\
- 'mutation', 'mutation_coordinate_start', 'mutation_coordinate_end',\
- 'allele_fraction', 'indellength']
-df = df.rename(columns = dict(enumerate(column_names)))
-
-shapecolor = {'SNV': 'rgba(0, 0, 0, 0.5)', 'insertion': 'rgba(239, 163, 64, 0.5)', 'deletion': 'rgba(112, 161, 239, 0.5)'}
-shapetype = {'SNV': 'circle', 'insertion': 'rect', 'deletion': 'rect'}
-guidename_log = set()
-plotdata_caller = {}
-plotdata_xaxisrange = []
-plotdata_guides = {}
-for a, callerdata in df.groupby(['caller']):
-    plotdata_scatter = []
-    for i, gDATA in callerdata.groupby(['guide', 'plate', 'well_position']):
-        for trow in gDATA.itertuples():
-            xaxisrange = [trow.guide_coordinate - 125, trow.guide_coordinate +125]
-            xvalues = [trow.mutation_coordinate_start, trow.mutation_coordinate_end]
-            yvalues = [trow.allele_fraction] * 2
-            hovertext = [', '.join(['well: '+'-'.join([trow.plate, trow.well_position]),
-                                    'length: '+ str(trow.indellength),
-                                    trow.guide])
-                        ] * 2
-            plotdata_scatter.append({
-                'x':xvalues,
-                'y':yvalues,
-                'name':trow.guide,
-                'mode':'lines',
-                'legendgroup':trow.guide,
-                'showlegend':False if trow.guide in guidename_log else True,
-                'line':{
-                    'color':shapecolor.get(trow.mutation),
-                    'width':5
-                },
-                'text': hovertext
-            })
-            guidename_log.update([trow.guide])
-            plotdata_xaxisrange.append(xaxisrange)
-    plotdata_caller.update({trow.caller:plotdata_scatter})
-
-
-fig = tools.make_subplots(rows = 1, cols = 2, subplot_titles = ['VarDict', 'HaplotypeCaller'])
-for i in plotdata_caller['VarDict']:
-    fig.append_trace(i, 1, 1)
-
-for j in plotdata_caller['HaplotypeCaller']:
-    fig.append_trace(j, 1, 2)
+fig = tools.make_subplots(rows = 1, cols = 2, subplot_titles = results_callers)
+col_index = 1
+for d_caller in plotdict:
+    for d_sample in plotdict[d_caller]:
+        for mut in plotdict[d_caller][d_sample]:
+            fig.append_trace(mut, 1, col_index)
+    col_index += 1
 
 for i in fig.layout:
     if i.find('xaxis') == 0:
@@ -130,8 +114,7 @@ for i in fig.layout:
     elif i.find('yaxis') == 0:
         fig.layout[i].update({'range':[0,1.1]})
 
-shapes = []
-annots = []
+shapes,annots = [],[]
 for caller in ['1','2']:
     for guidename,guidecoord in zip(guidedict, guidedict.values()):
         shape={
@@ -161,47 +144,3 @@ fig.layout.update({
 fig.layout['annotations'].extend(annots)
     
 py.plot(fig)
-
-
-#--------------using shapes instead
-#plotdata_scatter = []
-#plotdata_shapes = []
-#plotdata_xaxisrange = []
-#shapecolor = {'SNV': 'black', 'insertion': 'orange', 'deletion': 'steelblue'}
-#shapetype = {'SNV': 'circle', 'insertion': 'rect', 'deletion': 'rect'}
-#guidename_log = set()
-#for i, gDATA in df.groupby(['caller', 'guide', 'plate', 'well_position']):
-#    for trow in gDATA.itertuples():
-#        xaxisrange = [trow.guide_coordinate - 125, trow.guide_coordinate +125]
-#        plotdata_scatter.append({
-#            'x':xaxisrange,
-#            'y':[0,1],
-#            'name':trow.guide,
-#            'legendgroup':trow.guide,
-#            'showlegend':False if trow.guide in guidename_log else True,
-#            'text':'text',
-#            'mode':'text'
-#        })
-#        plotdata_xaxisrange.append(xaxisrange)
-#        plotdata_shapes.append({
-#            'type': shapetype.get(trow.mutation),
-#            'xref': 'x',
-#            'yref': 'y',
-#            'x0': trow.mutation_coordinate_start,
-#            'y0': trow.allele_fraction - 0.2,
-#            'x1': trow.mutation_coordinate_end,
-#            'y1': trow.allele_fraction,
-#            'line': {'width': 0},
-#            'fillcolor': shapecolor.get(trow.mutation),
-#            'opacity':0.5
-#        })
-#        guidename_log.update([trow.guide])
-#
-#plotdata_layout = {
-#        'xaxis':{'range':xaxisrange, 'showgrid':False},
-#        'yaxis':{'range':[0,1]},
-#        'shapes': plotdata_shapes
-#        }
-#
-#
-#py.plot(dict(data = plotdata_scatter, layout = plotdata_layout))
