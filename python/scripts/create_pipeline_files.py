3c17a71ca92c222aca94f408b5b7a98710d75106
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2017-12-14 15:53:46 +0000

    Create amplicons.txt and targets.txt based on the amplicons only.

--- python/scripts/create_pipeline_files.py
+++ python/scripts/create_pipeline_files.py
@@ -15,34 +15,47 @@ def convert_strand(s):
     return '*'
 
 
-def create_amplicon_file(session, project, seq_dict, driver_file):
-    copyfile(seq_dict, driver_file)
-    with open(driver_file, "a") as out:
-        selectionQuery = session.query(AmpliconSelection)\
+def create_files(session, project, seq_dict):
+    amp_file = "amplicons.txt"
+    target_file = "targets.txt"
+    
+    copyfile(seq_dict, amp_file)
+    copyfile(seq_dict, target_file)
+    
+    with open(amp_file, "a") as amp_out, open(target_file, "a") as tar_out:
+        ampliconQuery = session.query(Amplicon)\
                             .join(AmpliconSelection.amplicon)\
                             .join(AmpliconSelection.guide)\
                             .join(Guide.target)\
                             .join(Target.project)\
                             .filter(Project.geid == project)\
                             .order_by(Amplicon.chromosome.asc(), Amplicon.start.asc())
+        
+        # Should do this as another query with IN [amplicon.in_(Primer.amplicons], but it doesn't seem to work.
+        primers = session.query(Primer).all()
 
-        for s in selectionQuery.all():
-            a = s.amplicon
-            out.write("{}\t{}\t{}\t{}\t{}\n".format(a.chromosome, a.start, a.end, convert_strand(s.guide_strand),
-                                                    "{}_{}_{}".format(a.genome.assembly, a.chromosome, a.start)))
-
-
-def create_target_file(session, project, seq_dict, driver_file):
-    copyfile(seq_dict, driver_file)
-    with open(driver_file, "a") as out:
-        targetQuery = session.query(Target)\
-                             .join(Target.project)\
-                             .filter(Project.geid == project)\
-                             .order_by(Target.chromosome.asc(), Target.start.asc())
-
-        for t in targetQuery.all():
-            out.write("{}\t{}\t{}\t{}\t{}\n".format(t.chromosome, t.start, t.end, convert_strand(t.strand),
-                                                    "{}_{}_{}".format(t.genome.assembly, t.chromosome, t.start)))
+        for a in ampliconQuery.all():
+            
+            #print("Amplicon {} {} {}-{}".format(a.id, a.chromosome, a.start, a.end))
+                  
+            amp_out.write("{}\t{}\t{}\t+\t{}\n".format(a.chromosome, a.start, a.end,
+                                                        "{}_{}_{}".format(a.genome.assembly, a.chromosome, a.start)))
+            
+            aprimers = [ p for p in primers if a in p.amplicons ]
+            
+            tstart = a.start + 1
+            tend = a.end - 1
+                
+            for p in aprimers:
+                #print("Has primer {} {}-{} {} {}".format(p.id, p.start, p.end, p.strand, p.sequence))
+                
+                if p.strand == 'forward':
+                    tstart = p.start + 1
+                if p.strand == 'reverse':
+                    tend = p.end - 1
+            
+            tar_out.write("{}\t{}\t{}\t+\t{}\n".format(a.chromosome, tstart, tend,
+                                                        "{}_{}_{}".format(a.genome.assembly, a.chromosome, a.start)))
 
 
 def main():
@@ -62,9 +75,7 @@ def main():
     session = DBSession()
 
     try:
-        create_amplicon_file(session, options.project, options.dict, "amplicons.txt")
-        create_target_file(session, options.project, options.dict, "targets.txt")
-
+        create_files(session, options.project, options.dict)
         session.commit()
     except Exception as e:
         log.exception(e)
