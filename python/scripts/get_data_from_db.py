79859a8386706240c99a144e5cc6f5e26c0eadcf
pajanne <pajanne@gmail.com>
2018-01-11 17:25:28 +0000

    generate csv files instead of json to make it easier to read it back into R

--- python/scripts/get_data_from_db.py
+++ python/scripts/get_data_from_db.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-import json
+import csv
 
 from dnascissors.config import cfg
 from dnascissors.model import Base
@@ -36,13 +36,30 @@ for sequencing_library_content in results:
     row = {'well_id': well.id,
            'well_location': '{}_{}{}'.format(well.experiment_layout.geid, well.row, well.column),
            'sample_name': sequencing_library_content.sequencing_sample_name,
-           'abundances': [{'700': a.intensity_channel_700, '800': a.intensity_channel_800} for a in well.abundances],
+           'abundance_700': well.abundances[0].intensity_channel_700,
+           'abundance_800': well.abundances[0].intensity_channel_800,
            'growths': [(g.hours, g.confluence_percentage) for g in well.growths],
            'variant_results': [(v.variant_caller, v.variant_type, v.chromosome, v.position, v.ref, v.alt, v.allele_fraction, v.depth, v.amplicon, v.indel_length) for v in sequencing_library_content.variant_results],
            'mutation_summaries': [(m.zygosity, m.consequence, m.variant_caller_presence, m.score) for m in sequencing_library_content.mutation_summaries]
           }
     data.append(row)
 
-with open('data_from_db.json', 'w') as f:
+with open('GEP00001_abundances.csv', 'w') as csv_abundances,\
+     open('GEP00001_growths.csv', 'w') as csv_growths,\
+     open('GEP00001_mutation_summaries.csv', 'w') as csv_mutation_summaries,\
+     open('GEP00001_variant_results.csv', 'w') as csv_variant_results:
+    writer_csv_abundances = csv.DictWriter(csv_abundances, fieldnames=['well_id', 'well_location', 'sample_name', 'abundance_700', 'abundance_800'], extrasaction='ignore', delimiter='\t')
+    writer_csv_abundances.writeheader()
+    writer_csv_growths = csv.writer(csv_growths, delimiter='\t')
+    writer_csv_growths.writerow(['well_id', 'well_location', 'sample_name'] + ['growth_{}'.format(g[0]) for g in data[0]['growths']])
+    writer_csv_mutation_summaries = csv.writer(csv_mutation_summaries, delimiter='\t')
+    writer_csv_mutation_summaries.writerow(['well_id', 'well_location', 'sample_name', 'zygosity', 'merged_consequence', 'score'])
+    writer_variant_results = csv.writer(csv_variant_results, delimiter='\t')
+    writer_variant_results.writerow(['well_id', 'well_location', 'sample_name', 'variant_caller', 'variant_type', 'chromosome', 'position', 'ref', 'alt', 'allele_fraction', 'depth', 'amplicon', 'indel_length'])
     for d in data:
-        f.write(json.dumps(d, indent=2))
+        writer_csv_abundances.writerow(d)
+        writer_csv_growths.writerow([d['well_id'], d['well_location'], d['sample_name']] + [g[1] for g in d['growths']])
+        if len(d['mutation_summaries']) > 0:
+            writer_csv_mutation_summaries.writerow([d['well_id'], d['well_location'], d['sample_name'], d['mutation_summaries'][0][0], d['mutation_summaries'][0][1], d['mutation_summaries'][0][3]])
+        for variant_result in d['variant_results']:
+            writer_variant_results.writerow([d['well_id'], d['well_location'], d['sample_name']] + [v for v in variant_result])
