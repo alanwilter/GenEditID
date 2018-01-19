01f872d3ecbe2e8aa2b8b9d5f3d6730507794593
pajanne <pajanne@gmail.com>
2018-01-19 12:19:14 +0000

    add loader for newest project and a new script to load everything

---
+++ shell/load_all_projects.sh
@@ -0,0 +1,11 @@
+BASEDIR=$(dirname "$0")/..
+cd $BASEDIR
+
+source venv/bin/activate
+
+for projectid in GEP00001 GEP00002 GEP00003 GEP00004 GEP00005 GEP00006 GEP00007 GEP00009
+do
+  echo 'Loading project' ${projectid}
+  ./shell/load_project_${projectid}.sh
+  echo '-----------------------------------------------------------------------'
+done
