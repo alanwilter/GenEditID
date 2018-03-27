39050756c15fd5671f1e3d2e44896b17b87053c1
pajanne <pajanne@gmail.com>
2018-03-27 16:03:10 +0100

    add GEP00010 in the all projects loader

--- shell/load_all_projects.sh
+++ shell/load_all_projects.sh
@@ -3,7 +3,7 @@ cd $BASEDIR
 
 source venv/bin/activate
 
-for projectid in GEP00001 GEP00002 GEP00003 GEP00004 GEP00005 GEP00006 GEP00007 GEP00009
+for projectid in GEP00001 GEP00002 GEP00003 GEP00004 GEP00005 GEP00006 GEP00007 GEP00009 GEP00010
 do
   echo 'Loading project' ${projectid}
   ./shell/load_project_${projectid}.sh
