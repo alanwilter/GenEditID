464ad52c0f484209d1ac02ec5f6a3f893af26730
pajanne <pajanne@gmail.com>
2019-03-22 18:48:14 +0000

    add variant plot - still work in progress

--- python/scripts/ampliplot.py
+++ python/scripts/ampliplot.py
@@ -1,27 +1,27 @@
 import plotly.offline as py
 import pandas as pd
 
-df = pd.read_csv('amplicount_coverage.csv')
+df_coverage = pd.read_csv('amplicount_coverage.csv')
 
-filter_columns = [col for col in df if col.startswith('chr')]
-print(filter_columns)
-df['misc'] = df['total_reads'] - df[filter_columns].sum(axis=1)
-print(df.head())
+amlicons_ids = [col for col in df_coverage if col.startswith('chr')]
+print(amlicons_ids)
+df_coverage['misc'] = df_coverage['total_reads'] - df_coverage[amlicons_ids].sum(axis=1)
+print(df_coverage.head())
 
 data = []
-for col in filter_columns:
+for amplicon_id in amlicons_ids:
     trace = {
-        'x': df[col],
-        'y': df['barcode'],
-        'name': col,
+        'x': df_coverage[amplicon_id],
+        'y': df_coverage['barcode'],
+        'name': amplicon_id,
         'type': 'bar',
         'orientation': 'h'
     }
     data.append(trace)
 
 misc_trace = {
-    'x': df['misc'],
-    'y': df['barcode'],
+    'x': df_coverage['misc'],
+    'y': df_coverage['barcode'],
     'name': 'misc',
     'type': 'bar',
     'orientation': 'h',
@@ -36,5 +36,47 @@ layout = {'barmode': 'stack',
 
 py.plot({'data': data, 'layout': layout}, filename='ampliplot')
 
-df = pd.read_csv('amplicount.csv')
-print(df.head())
+df_variants = pd.read_csv('amplicount.csv')
+df_variants['len'] = df_variants['sequence'].str.len()
+
+print(df_variants.head())
+
+for amplicon_id in amlicons_ids:
+    df_barcodes = df_coverage[['barcode']]
+    #df_barcodes['value'] = 0
+    print(amplicon_id)
+    filter_df_variants = df_variants[(df_variants['amplicon_id'] == amplicon_id) &\
+                                     (df_variants['len'] > 50)]
+
+    filter_df_variants = filter_df_variants[['barcode', 'sequence', 'variant_frequency']]
+    #filter_df_variants = filter_df_variants.merge(df_barcodes, left_on='barcode', right_on='barcode', how='outer')
+    print(filter_df_variants)
+    pivot_df_variants = filter_df_variants.pivot(index='barcode', columns='sequence', values='variant_frequency').reset_index()
+    pivot_df_variants.fillna(value=0, inplace=True)
+    pivot_df_variants['other'] = 100 - pivot_df_variants.iloc[:, 1:].sum(axis=1)
+    pivot_df_variants = pivot_df_variants.merge(df_barcodes, left_on='barcode', right_on='barcode', how='outer')
+    pivot_df_variants.fillna(value=0, inplace=True)
+    print(pivot_df_variants.head())
+
+    data = []
+    i = 0
+    for seq in pivot_df_variants.iloc[:, 1:].columns.tolist():
+        i += 1
+        print(seq)
+        if seq == 'other':
+            name = seq
+        else:
+            name = 'variant_{}'.format(i)
+        trace = {
+            'x': pivot_df_variants[seq],
+            'y': pivot_df_variants['barcode'],
+            'name': name,
+            'type': 'bar',
+            'orientation': 'h'
+        }
+        data.append(trace)
+    layout = {'barmode': 'stack',
+              'title': 'Variant Read Coverage for Amplicon {}'.format(amplicon_id),
+              'xaxis': {'title': 'frequency of reads'},
+              'yaxis': {'title': 'samples'}}
+    py.plot({'data': data, 'layout': layout}, filename='{}_ampliplot'.format(amplicon_id))
