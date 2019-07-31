0515eaffbe9b909fab9a52cd478863413edef20a
Anne Pajon <pajanne@gmail.com>
2019-07-31 10:59:31 +0100

    sort out symbolic issue (hopefully)

--- shell/setup_geneditid.sh
+++ shell/setup_geneditid.sh
@@ -21,4 +21,6 @@ echo 'Installing Homo Sapiens reference genome'
 pyensembl install --release 95 --species homo_sapiens
 
 echo 'Create symlink to template file for accessing it from the WebApp'
-ln -s data/templates/GEPXXXXX.xlsx python/webapp/static
+cd python/webapp/static/
+ln -s ../../../data/templates/GEPXXXXX.xlsx
+cd $BASEDIR
