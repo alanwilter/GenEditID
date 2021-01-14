6231170785e461c12f5c0813c00fc0182789efbd
Anne Pajon <pajanne@gmail.com>
2021-01-14 22:26:55 +0100

    update script to join paired-end reads to use pear instead of fastq-join

--- scripts/run_joinreads.sh
+++ scripts/run_joinreads.sh
@@ -11,14 +11,15 @@ echo `pwd`
 for f in *.s_1.r_1.fq.gz
   do
     echo $f
-    fastq-join $f `echo $f | cut -d'.' -f1-4`.r_2.fq.gz -o `echo $f | cut -d'.' -f1-4`.fq
+    pear -f $f -r `echo $f | cut -d'.' -f1-4`.r_2.fq.gz -o `echo $f | cut -d'.' -f1-4`
   done
 
-for f in *.fqjoin
+for f in *.assembled.fastq
   do
     gzip $f
+    mv $f.gz `echo $f | cut -d'.' -f1-4`.fqjoin.gz
   done
 
-rm *.fqun?
+rm *.discarded.* *.unassembled.*
 
 echo 'Job done'
